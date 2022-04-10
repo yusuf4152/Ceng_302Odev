6bfd3c60e7d69fda5e9bd79e90b92c1d12745509
yusuf4152 <yusufislamdemir41@gmail.com>
2022-04-10 17:41:03 +0300

    ilk sürüm(toplama)

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import hesapla.HesapMakinesi;
+
+public class Main {
+    public static void main(String[] args) {
+        HesapMakinesi hesapMakinesi = new HesapMakinesi();
+        System.out.println(hesapMakinesi.toplama(1,2));
+    }
+}
---
+++ src/main/java/hesapla/HesapMakinesi.java
@@ -0,0 +1,8 @@
+package hesapla;
+
+public class HesapMakinesi {
+    public int toplama(int sayi1, int sayi2){
+        return sayi1 + sayi2;
+    }
+
+}
