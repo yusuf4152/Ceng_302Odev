f620c1bc6a6ad5fad640ee4b7460cb6e44a132fc
yusuf4152 <yusufislamdemir41@gmail.com>
2022-04-10 17:43:54 +0300

    ikinci sürüm(bolme)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         HesapMakinesi hesapMakinesi = new HesapMakinesi();
         System.out.println(hesapMakinesi.toplama(1,2));
+        System.out.println(hesapMakinesi.bolme(2,2));
     }
 }
--- src/main/java/hesapla/HesapMakinesi.java
+++ src/main/java/hesapla/HesapMakinesi.java
@@ -4,5 +4,8 @@ public class HesapMakinesi {
     public int toplama(int sayi1, int sayi2){
         return sayi1 + sayi2;
     }
+    public int bolme(int sayi1, int sayi2){
+        return sayi1/sayi2;
+    }
 
 }
